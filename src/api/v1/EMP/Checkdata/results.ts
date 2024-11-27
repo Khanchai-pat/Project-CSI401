0241cca06138f494820c20f7ce543d86b08a679f
jirawatChn <jirawat.chn@spumail.net>
2024-11-27 16:49:44 +0700

    update checkdata dahsboard

--- src/api/v1/EMP/Checkdata/results.ts
+++ src/api/v1/EMP/Checkdata/results.ts
@@ -1,45 +1,44 @@
-import express, { Request, Response } from 'express';
-import { responseData, responseError } from '../../interfaceRes/response'
-import { employees } from '../../Schema/emp';
+import express, { Request, Response } from "express";
+import { responseData, responseError } from "../../interfaceRes/response";
+import { employees } from "../../Schema/emp";
+import { courseResult } from "../../HR/courseResults/route";
+import { courseResults } from "../../Schema/courseResults";
 
 export const checkdata = express();
 
+checkdata.post("/checkdata", async (req: Request, res: Response) => {
+  const reqHeader: any = req.headers;
+  const contentType: any = reqHeader["content-type"];
+  const tokenkey: any = reqHeader["authorization"];
+  // const empID = req.query.Empid as string;
+  const { empID } = req.body;
 
-
-
-checkdata.post('/checkdata', async (req: Request, res: Response) => {
-    const reqHeader: any = req.headers;
-    const contentType: any = reqHeader["content-type"];
-    const tokenkey: any = reqHeader["authorization"];
-    // const empID = req.query.Empid as string;
-    const { empID } = req.body
-
-
-    // ตรวจสอบการมี empID ในคำขอและเช็ค contentType
-    if (!tokenkey || !contentType) {
-        res.status(400).json({
-            code: "400",
-            status: "Bad Request",
-            message: "Cannot Show "
-        });
-    }
-    else if (!empID){
-        res.status(404).json({
-            code: "404",
-            status: "error",
-            message: "EmpID not found",
-          });
-    }  
-    // สร้างข้อมูล response
-    const CheckdataresponseData = await employees.findOne({
-        empID: empID
+  // ตรวจสอบการมี empID ในคำขอและเช็ค contentType
+  if (!tokenkey || !contentType) {
+    res.status(400).json({
+      code: "400",
+      status: "Bad Request",
+      message: "Cannot Show ",
     });
-  
+  } else if (!empID) {
+    res.status(404).json({
+      code: "404",
+      status: "error",
+      message: "EmpID not found",
+    });
+  } else {
+    // สร้างข้อมูล response
+    const empData = await employees.findOne({
+      empID: empID,
+    });
+    const courseResult = await courseResults
+      .find({ empID: empID })
+      .sort({ _id: -1 });
+
     res.status(200).json({
-        code: "200",
-        status: "success",
-        data: { CheckdataresponseData }
-
-    })
+      code: "200",
+      status: "success",
+      data: { empData, courseResult },
+    });
+  }
 });
-
