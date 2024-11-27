4c82e304200d0b56ee7440abf1b2953cb912577e
65061447 <smith.dip@spumail.net>
2024-11-27 16:59:04 +0700

    enrollment

--- src/api/v1/EMP/Checkdata/results.ts
+++ src/api/v1/EMP/Checkdata/results.ts
@@ -3,10 +3,11 @@ import { responseData, responseError } from "../../interfaceRes/response";
 import { employees } from "../../Schema/emp";
 import { courseResult } from "../../HR/courseResults/route";
 import { courseResults } from "../../Schema/courseResults";
+import { enrollments } from "../../Schema/enrollment";
 
 export const checkdata = express();
 
-checkdata.post("/checkdata", async (req: Request, res: Response) => {
+checkdata.post("/dashboard", async (req: Request, res: Response) => {
   const reqHeader: any = req.headers;
   const contentType: any = reqHeader["content-type"];
   const tokenkey: any = reqHeader["authorization"];
@@ -39,6 +40,38 @@ checkdata.post("/checkdata", async (req: Request, res: Response) => {
       code: "200",
       status: "success",
       data: { empData, courseResult },
+    });
+  }
+});
+
+checkdata.post("/enrollments", async (req: Request, res: Response) => {
+  const reqHeader: any = req.headers;
+  const contentType: any = reqHeader["content-type"];
+  const tokenkey: any = reqHeader["authorization"];
+  // const empID = req.query.Empid as string;
+  const { empID} = req.body;
+
+  // ตรวจสอบการมี empID ในคำขอและเช็ค contentType
+  if (!tokenkey || !contentType) {
+    res.status(400).json({
+      code: "400",
+      status: "Bad Request",
+      message: "Cannot Show ",
+    });
+  } else if (!empID) {
+    res.status(404).json({
+      code: "404",
+      status: "error",
+      message: "EmpID not found",
+    });
+  } else {
+    // สร้างข้อมูล response
+    const enrollment = await enrollments.find({empId : empID,status : "registered"})
+
+    res.status(200).json({
+      code: "200",
+      status: "success",
+      data: {enrollment},
     });
   }
 });
